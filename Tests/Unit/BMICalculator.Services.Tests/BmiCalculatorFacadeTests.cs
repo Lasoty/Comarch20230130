420b0bcfb40b68e0907a14c95c41ec712892bc28
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-01-31 10:28:53 +0100

    Moq Returns

---
+++ Tests/Unit/BMICalculator.Services.Tests/BmiCalculatorFacadeTests.cs
@@ -0,0 +1,46 @@
+﻿using BMICalculator.Model.DTO;
+using BMICalculator.Services.Enums;
+using BMICalculator.Services.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator.Services.Tests
+{
+    public class BmiCalculatorFacadeTests
+    {
+        Mock<IBmiCalculatorFactory> bmiCalculatorFactoryMock;
+        Mock<IBmiDeterminator> bmiDeterminatorMock;
+
+        IBmiCalculatorFacade bmiCalculatorFacade;
+
+        [SetUp]
+        public void SetUp()
+        {
+            bmiDeterminatorMock= new Mock<IBmiDeterminator>();
+            bmiCalculatorFactoryMock = new Mock<IBmiCalculatorFactory>();
+
+            bmiCalculatorFacade = new BmiCalculatorFacade(bmiDeterminatorMock.Object, bmiCalculatorFactoryMock.Object);
+        }
+
+
+        [Test]
+        public void GetResultShouldResultUnderweightWhenWeightIsToLow() 
+        {
+            // Arrange
+            bmiCalculatorFactoryMock.Setup(m => m.CreateCalculator(It.IsAny<UnitSystem>()))
+                .Returns(new MetricBmiCalculator());
+
+            // Act
+            var actual = bmiCalculatorFacade.GetResult(50, 200, UnitSystem.Metric);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.AreEqual(BmiClassification.Underweight, actual.BmiClassification);
+        }
+    }
+}
